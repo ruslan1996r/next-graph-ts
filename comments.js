28aba15a0b8fee96785d6d982ba8d540c4b16384
ruslan1996r <dshmc27@gmail.com>
2021-07-31 17:07:49 +0300

    Сервер и клиент в одном репозитории. Рабочая авторизация через URQL

---
+++ comments.js
@@ -0,0 +1,14 @@
+
+/**
+ * * Стараться комментировать таким образом
+ * ! Deprecated method
+ * ? Should I delete?
+ * TODO: Использовать эти комментарии в SMALL SHOP
+ *
+ * @param params
+ * @returns {string}
+ */
+
+function myFn(params) {
+  return params
+}
--- src/types.ts
+++
@@ -1,9 +0,0 @@
-import { EntityManager, IDatabaseDriver, Connection } from "@mikro-orm/core";
-import { Request, Response } from "express";
-
-
-export type MyContext = {
-  em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>;
-  req: Request & { session: any }; // Express.Session. Этот тип козлит из-за версии библиотеки
-  res: Response;
-};
